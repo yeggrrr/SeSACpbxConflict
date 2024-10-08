e762c41d929ed6fc8240ab16a974d0714239acce
yeggrrr <yjkim981103@gmail.com>
2024-10-08 11:17:50 +0900

    start

---
+++ SeSACpbxConflict/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SeSACpbxConflict
+//
+//  Created by YJ on 10/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
